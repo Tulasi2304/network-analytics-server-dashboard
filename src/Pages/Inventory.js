ea774dd537418c4142c17a05d8dab131d4d1e4b4
Tulasi2304 <tulasi.udupa23@gmail.com>
2025-02-19 14:47:28 +0530

    Change table design

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -1,112 +1,92 @@
 import React, { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid2";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-// import data from "../data/dummyData.json";
+import {
+    Box,
+    Typography,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Grid,
+    Card,
+    CardContent,
+    IconButton,
+    Menu,
+    MenuItem,
+} from "@mui/material";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 export default function Inventory() {
-
     const [data, setData] = useState(null);
+    const [selectedDeviceType, setSelectedDeviceType] = useState("");
+    const [anchorEl, setAnchorEl] = useState(null);
 
-    // const [data, setData] = useState({
-    //     devices: [],
-    //     metrics: [],
-    //     alerts: [],
-    //     analysisResults: []
-    // });
-
-    // useEffect(() => {
-    //     fetch("http://localhost:8080/api/data")  // Replace with your actual API endpoint
-    //         .then(response => response.json())
-    //         .then(fetchedData => {
-    //             setData({
-    //                 devices: fetchedData.devices || [],
-    //                 metrics: fetchedData.metrics || [],
-    //                 alerts: fetchedData.alerts || [],
-    //                 analysisResults: fetchedData.analysisResults || []
-    //             });
-    //         })
-    //         .catch(error => console.error("Error fetching data:", error));
-    // }, []);
-    
-    
     useEffect(() => {
         fetch("http://localhost:8081/devices")
-          .then((res) => res.json())
-          .then((data) => setData(data))
-          .catch((err) => console.log(err));
-      }, []);
-    
-    // console.log(data);
+            .then((res) => res.json())
+            .then((data) => setData(data))
+            .catch((err) => console.log(err));
+    }, []);
 
-    const [selectedDeviceType, setSelectedDeviceType] = useState("");
-
-    const handleChange = (event) => {
-        setSelectedDeviceType(event.target.value);
+    const handleFilterClick = (event) => {
+        setAnchorEl(event.currentTarget);
     };
 
-    // const deviceTypes = [...new Set(data?.devices.map(device => device.deviceType))];
-    const deviceTypes = [...new Set(data?.map(device => device.deviceType))];
+    const handleFilterClose = (deviceType) => {
+        setSelectedDeviceType(deviceType);
+        setAnchorEl(null);
+    };
 
-    const selectedDevices = data?.filter(device => selectedDeviceType === "" || device.deviceType === selectedDeviceType); // for filtering
-    // const selectedAnalysis = data?.analysisResults.filter(result => selectedDevices.some(device => device.id === result.deviceId));
-    // const selectedAlerts = data?.alerts.filter(alert => selectedDevices.some(device => device.id === alert.deviceId));
+    const deviceTypes = [...new Set(data?.map((device) => device.deviceType))];
 
-    let deviceHeaders = null;
-    if(data){
-        deviceHeaders = Object.keys(data[0]);
-    }
-    // const analysisHeaders = Object.keys(data?.analysisResults[0]);
-    // const alertHeaders = Object.keys(data?.alerts[0]);
+    const selectedDevices = data?.filter(
+        (device) => selectedDeviceType === "" || device.deviceType === selectedDeviceType
+    );
+
+    let deviceHeaders = data ? Object.keys(data[0]) : [];
 
     return (
         <Box sx={{ marginTop: 8, padding: 4 }}>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Card>
-                        <CardContent>
-                            <Typography gutterBottom variant="h6">Select a Device Type:</Typography>
-                            <FormControl sx={{ minWidth: 200 }}>
-                                <InputLabel>Device Type</InputLabel>
-                                <Select value={selectedDeviceType} onChange={handleChange} label="Device Type">
-                                    <MenuItem value="">All</MenuItem>
-                                    {deviceTypes.map(type => (
-                                        <MenuItem key={type} value={type}>{type}</MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
+                        <CardContent sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                            <Typography gutterBottom variant="h6">
+                                Device Inventory
+                            </Typography>
+                            <IconButton onClick={handleFilterClick}>
+                                <FilterListIcon sx={{color: "#212121"}}/>
+                            </IconButton>
+                            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => handleFilterClose("")}>
+                                <MenuItem onClick={() => handleFilterClose("")}>All</MenuItem>
+                                {deviceTypes.map((type) => (
+                                    <MenuItem key={type} onClick={() => handleFilterClose(type)}>
+                                        {type}
+                                    </MenuItem>
+                                ))}
+                            </Menu>
                         </CardContent>
                     </Card>
                 </Grid>
 
                 <Grid item xs={12}>
-                    <Typography variant="h6">Device Inventory</Typography>
                     <TableContainer component={Paper}>
                         <Table>
                             <TableHead>
-                                <TableRow>
-                                    {deviceHeaders?.map(header => (
-                                        <TableCell key={header}>{header}</TableCell>
+                                <TableRow sx={{ backgroundColor: "#a9cff5", color: "#212121" }}>
+                                    {deviceHeaders?.map((header) => (
+                                        <TableCell key={header} sx={{ color: "#212121", fontWeight: "bold" }}>
+                                            {header}
+                                        </TableCell>
                                     ))}
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {selectedDevices?.map(device => (
-                                    <TableRow key={device.id}>
-                                        {deviceHeaders?.map(header => (
+                                {selectedDevices?.map((device, index) => (
+                                    <TableRow key={device.id} sx={{ backgroundColor: index % 2 === 0 ? "#f5f5f5" : "white" }}>
+                                        {deviceHeaders?.map((header) => (
                                             <TableCell key={header}>{device[header]}</TableCell>
                                         ))}
                                     </TableRow>
@@ -115,54 +95,6 @@ export default function Inventory() {
                         </Table>
                     </TableContainer>
                 </Grid>
-
-                {/* <Grid item xs={12}>
-                    <Typography variant="h6">Analysis Results</Typography>
-                    <TableContainer component={Paper}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    {analysisHeaders.map(header => (
-                                        <TableCell key={header}>{header}</TableCell>
-                                    ))}
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {selectedAnalysis.map(result => (
-                                    <TableRow key={result.id}>
-                                        {analysisHeaders.map(header => (
-                                            <TableCell key={header}>{result[header]}</TableCell>
-                                        ))}
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Typography variant="h6">Alerts</Typography>
-                    <TableContainer component={Paper}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    {alertHeaders.map(header => (
-                                        <TableCell key={header}>{header}</TableCell>
-                                    ))}
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {selectedAlerts.map(alert => (
-                                    <TableRow key={alert.id}>
-                                        {alertHeaders.map(header => (
-                                            <TableCell key={header}>{alert[header]}</TableCell>
-                                        ))}
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Grid> */}
             </Grid>
         </Box>
     );
--- src/Pages/TrendsAndAlerts.js
+++ src/Pages/TrendsAndAlerts.js
@@ -1,154 +1,128 @@
 import React, { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid2";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-// import data from "../data/dummyData.json";
+import {
+    Box,
+    Typography,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Grid,
+    Card,
+    CardContent,
+    IconButton,
+    Menu,
+    MenuItem,
+} from "@mui/material";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 export default function Inventory() {
-
-    const [data, setData] = useState({
-        devices: [],
-        alerts: [],
-        analysis: []
-    });
+    const [data, setData] = useState({ devices: [], alerts: [], analysis: [] });
+    const [selectedDeviceType, setSelectedDeviceType] = useState("");
+    const [anchorEl, setAnchorEl] = useState(null);
 
     useEffect(() => {
-        fetch("http://localhost:8081/devices")  // Replace with your actual API endpoint
-            .then(response => response.json())
-            .then(fetcheddevices => {
-                setData(prevData => ({
-                    ...prevData,
-                    devices: fetcheddevices || []
-                }));
-            })
-            .catch(error => console.error("Error fetching analysis:", error));
+        fetch("http://localhost:8081/devices")
+            .then((res) => res.json())
+            .then((devices) => setData((prev) => ({ ...prev, devices })))
+            .catch((err) => console.error("Error fetching devices:", err));
 
-        fetch("http://localhost:8081/analysis")  // Replace with your actual API endpoint
-            .then(response => response.json())
-            .then(fetchedAnalysis => {
-                setData(prevData => ({
-                    ...prevData,
-                    analysis: fetchedAnalysis || []
-                }));
-            })
-            .catch(error => console.error("Error fetching analysis:", error));
+        fetch("http://localhost:8081/analysis")
+            .then((res) => res.json())
+            .then((analysis) => setData((prev) => ({ ...prev, analysis })))
+            .catch((err) => console.error("Error fetching analysis:", err));
 
         fetch("http://localhost:8081/alerts")
-            .then(response => response.json())
-            .then(fetchedAlerts => {
-                setData(prevData => ({
-                    ...prevData,
-                    alerts: fetchedAlerts || []
-                }));
-            })
-            .catch(error => console.error("Error fetching alerts:", error));
+            .then((res) => res.json())
+            .then((alerts) => setData((prev) => ({ ...prev, alerts })))
+            .catch((err) => console.error("Error fetching alerts:", err));
     }, []);
 
-    console.log(data);
-    console.log("Fetched Analysis Data:", data.analysis[1]);
-    console.log("Fetched Devices Data:", data.devices);
-
-
-    const [selectedDeviceType, setSelectedDeviceType] = useState("");
-
-    const handleChange = (event) => {
-        setSelectedDeviceType(event.target.value);
+    const handleFilterClick = (event) => {
+        setAnchorEl(event.currentTarget);
     };
 
-    const deviceTypes = [...new Set(data?.devices?.map(device => device.deviceType))];
+    const handleFilterClose = (deviceType) => {
+        setSelectedDeviceType(deviceType);
+        setAnchorEl(null);
+    };
 
-    const selectedDevices = data?.devices?.filter(device => selectedDeviceType === "" || device.deviceType === selectedDeviceType); // for filtering
-    const selectedAnalysis = data?.analysis?.filter(result => selectedDevices.some(device => device.id === result.device.id));
-    const selectedAlerts = data?.alerts?.filter(alert => selectedDevices.some(device => device.id === alert.deviceId));
+    const deviceTypes = [...new Set(data.devices.map((d) => d.deviceType))];
+    const filteredDevices = data.devices.filter(
+        (d) => !selectedDeviceType || d.deviceType === selectedDeviceType
+    );
+    const filteredAnalysis = data.analysis.filter((a) =>
+        filteredDevices.some((d) => d.id === a.device.id)
+    );
+    const filteredAlerts = data.alerts.filter((a) =>
+        filteredDevices.some((d) => d.id === a.deviceId)
+    );
 
-    console.log("Selected analysis:", selectedAnalysis)
-
-    let analysisHeaders = data?.analysis?.length > 0 ? Object.keys(data.analysis[0]) : [];
-
-    let alertHeaders = data?.alerts?.length > 0 ? Object.keys(data.alerts[0]) : [];
+    const analysisHeaders = data.analysis.length > 0 ? Object.keys(data.analysis[0]) : [];
+    const alertHeaders = data.alerts.length > 0 ? Object.keys(data.alerts[0]) : [];
 
     return (
         <Box sx={{ marginTop: 8, padding: 4 }}>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Card>
-                        <CardContent>
-                            <Typography gutterBottom variant="h6">Select a Device Type:</Typography>
-                            <FormControl sx={{ minWidth: 200 }}>
-                                <InputLabel>Device Type</InputLabel>
-                                <Select value={selectedDeviceType} onChange={handleChange} label="Device Type">
-                                    <MenuItem value="">All</MenuItem>
-                                    {deviceTypes.map(type => (
-                                        <MenuItem key={type} value={type}>{type}</MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
+                        <CardContent sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                            <Typography variant="h6">Trends and Alerts</Typography>
+                            <IconButton onClick={handleFilterClick}>
+                                <FilterListIcon sx={{color: "#212121"}} />
+                            </IconButton>
+                            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={() => handleFilterClose("")}>
+                                <MenuItem onClick={() => handleFilterClose("")}>All</MenuItem>
+                                {deviceTypes.map((type) => (
+                                    <MenuItem key={type} onClick={() => handleFilterClose(type)}>{type}</MenuItem>
+                                ))}
+                            </Menu>
                         </CardContent>
                     </Card>
                 </Grid>
 
                 <Grid item xs={12}>
-                    <Typography variant="h6">Analysis Results</Typography>
+                    <Typography variant="h6" sx={{m: 1}}>Analysis Results</Typography>
                     <TableContainer component={Paper}>
                         <Table>
-                            <TableHead>
-                                <TableRow>
-                                    {analysisHeaders?.map(header => (
-                                        <TableCell key={header}>{header}</TableCell>
-                                    ))}
-                                </TableRow>
-                            </TableHead>
+                            <TableRow sx={{ backgroundColor: "#a9cff5", color: "#212121" }}>
+                                {analysisHeaders?.map((header) => (
+                                    <TableCell key={header} sx={{ color: "#212121", fontWeight: "bold" }}>
+                                        {header}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
                             <TableBody>
-                                {selectedAnalysis?.map(result => (
-                                    <TableRow key={result.id}>
-                                        {analysisHeaders?.map(header => (
-                                            <TableCell key={header}>
-                                                {typeof result[header] === "object"
-                                                    ? result[header]?.id // Convert object to string
-                                                    : result[header]}
-                                            </TableCell>
+                                {filteredAnalysis.map((result, index) => (
+                                    <TableRow key={result.id} sx={{ backgroundColor: index % 2 === 0 ? "#f5f5f5" : "white" }}>
+                                        {analysisHeaders.map((header) => (
+                                            <TableCell key={header}>{typeof result[header] === "object" ? result[header]?.id : result[header]}</TableCell>
                                         ))}
                                     </TableRow>
                                 ))}
                             </TableBody>
-
                         </Table>
                     </TableContainer>
                 </Grid>
 
                 <Grid item xs={12}>
-                    <Typography variant="h6">Alerts</Typography>
+                    <Typography variant="h6" sx={{m: 1}}>Alerts</Typography>
                     <TableContainer component={Paper}>
                         <Table>
-                            <TableHead>
-                                <TableRow>
-                                    {alertHeaders?.map(header => (
-                                        <TableCell key={header}>{header}</TableCell>
-                                    ))}
-                                </TableRow>
-                            </TableHead>
+                            <TableRow sx={{ backgroundColor: "#a9cff5", color: "#212121" }}>
+                                {alertHeaders?.map((header) => (
+                                    <TableCell key={header} sx={{ color: "#212121", fontWeight: "bold" }}>
+                                        {header}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
                             <TableBody>
-                                {selectedAlerts?.map(alert => (
-                                    <TableRow key={alert.id}>
-                                        {alertHeaders?.map(header => (
-                                            <TableCell key={header}>
-                                                {typeof alert[header] === "object"
-                                                    ? alert[header]?.id // Show device ID if it's an object
-                                                    : alert[header]}
-                                            </TableCell>
+                                {filteredAlerts.map((alert, index) => (
+                                    <TableRow key={alert.id} sx={{ backgroundColor: index % 2 === 0 ? "#f5f5f5" : "white" }}>
+                                        {alertHeaders.map((header) => (
+                                            <TableCell key={header}>{typeof alert[header] === "object" ? alert[header]?.id : alert[header]}</TableCell>
                                         ))}
                                     </TableRow>
                                 ))}
